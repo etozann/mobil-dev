ff61c1e9834b88cf96e3af14213a58667176d73c
etozann <73287066+etozann@users.noreply.github.com>
2022-11-07 17:54:41 +0300

    Add files via upload

---
+++ lib/MasterDetailPage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:odev5_mp/DetailPage.dart';
+import 'package:odev5_mp/DetailWidget.dart';
+import 'package:odev5_mp/ListWidget.dart';
+
+class MasterDetailPage extends StatefulWidget {
+  @override
+  _MasterDetailPageState createState() => _MasterDetailPageState();
+}
+
+class _MasterDetailPageState extends State<MasterDetailPage> {
+  var selectedValue = 0;
+  var isLargeScreen = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: OrientationBuilder(builder: (context, orientation) {
+        if (MediaQuery.of(context).size.width > 600) {
+          isLargeScreen = true;
+        } else {
+          isLargeScreen = false;
+        }
+
+        return Row(children: <Widget>[
+          Expanded(
+            child: ListWidget(21, (value) {
+              if (isLargeScreen) {
+                selectedValue = value;
+                setState(() {});
+              } else {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return DetailPage(value);
+                  },
+                ));
+              }
+            }),
+          ),
+          isLargeScreen
+              ? Expanded(child: DetailWidget(selectedValue))
+              : Container(),
+        ]);
+      }),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:odev5_mp/MasterDetailPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MasterDetailPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
